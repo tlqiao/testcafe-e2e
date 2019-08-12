c71986eebbaf42c4a1f14765dfd907ae73c6eeb0
tlqiao <qiaotl@163.com>
2019-08-12 21:41:57 +0800

    update it

--- e2e/scenario/article/case/postNewArticleSpec.js
+++ e2e/scenario/article/case/postNewArticleSpec.js
@@ -1,5 +1,5 @@
-import menu from '../util/menu'
-import newArticle from './pages/newArticlePage'
+import menu from '../../../util/menu'
+import newArticle from '../pages/newArticlePage'
 fixture('publish new article test')
 test('should publish new article test', async (t) => {
    await menu.login(t)
